5bae937e9ae93a25e80e9edfe820738f15cc841b
Baeri3000 <lars.stalder@gmail.com>
2024-01-09 13:25:02 +0100

    restyle links

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -74,7 +74,7 @@ onMounted(() => {
         <ul class="play nav-content desktop-view">
           <li @click="reset">
             <div>
-              <a class="btn1" href="" target="_blank"
+              <a class="btn2" href="" target="_blank"
                  aria-label="Contact us">Contact us</a>
             </div>
           </li>
