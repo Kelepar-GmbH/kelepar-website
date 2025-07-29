d58624de9943eb70a07338d05b514fe672b859e7
Lars Stalder <lars@kelepar.com>
2025-07-29 20:20:53 +0200

    update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import Home from '../views/Home.vue'
-import Contact from '../views/Contact.vue'
-import Privacy from '../views/Privacy.vue'
-import NotFound from '../views/NotFound.vue'
+import Home from '@/views/Home.vue'
+import Contact from '@/views/Contact.vue'
+import Privacy from '@/views/Privacy.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
